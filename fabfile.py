5bf746c290a08f2883426f07568b464f823b3307
Nolan Nichols <nolan.nichols@gmail.com>
2013-11-10 13:56:51 -0800

    add: modify fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,14 @@
-from fabric.api import *
-import fabric.contrib.project as project
 import os
 
-# Local path configuration (can be absolute or relative to fabfile)
-env.deploy_path = 'output'
-DEPLOY_PATH = env.deploy_path
-
-# Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
-
+from fabric.api import local
 
 def clean():
-    if os.path.isdir(DEPLOY_PATH):
-        local('rm -rf {deploy_path}'.format(**env))
-        local('mkdir {deploy_path}'.format(**env))
+    if os.path.isdir('output'):
+        local('rm -rf output')
+        local('mkdir output')
 
 def build():
-    local('pelican -s pelicanconf.py')
+    local('pelican content -o output -s pelicanconf.py')
 
 def rebuild():
     clean()
@@ -32,29 +18,14 @@ def regenerate():
     local('pelican -r -s pelicanconf.py')
 
 def serve():
-    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+    local('cd output && python -m SimpleHTTPServer')
 
 def reserve():
     build()
     serve()
 
-def preview():
-    local('pelican -s publishconf.py')
-
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
-
-@hosts(production)
 def publish():
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
+    local('pelican content -o output -s publishconf.py')
+    local('ghp-import output')
+    local('git push origin gh-pages:master')
+
