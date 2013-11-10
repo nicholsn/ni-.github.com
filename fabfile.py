a1e7bbb229e468a0cfe73672db2af24bd3a99fb8
Nolan Nichols <nolan.nichols@gmail.com>
2013-11-10 14:40:47 -0800

    fix: fabfile commands for gh

--- fabfile.py
+++ fabfile.py
@@ -6,26 +6,26 @@ def clean():
     if os.path.isdir('output'):
         local('rm -rf output')
         local('mkdir output')
+    else:
+        local('mkdir output')
 
 def build():
+    clean()
     local('pelican content -o output -s pelicanconf.py')
 
 def rebuild():
     clean()
     build()
 
-def regenerate():
+def autobuild():
     local('pelican -r -s pelicanconf.py')
 
 def serve():
-    local('cd output && python -m SimpleHTTPServer')
-
-def reserve():
-    build()
-    serve()
+    local('cd output && open http://localhost:8000 '
+          '&& python -m SimpleHTTPServer')
 
 def publish():
     local('pelican content -o output -s publishconf.py')
     local('ghp-import output')
-    local('git push origin gh-pages:master')
+    local('git push upstream  gh-pages:master --force')
 
